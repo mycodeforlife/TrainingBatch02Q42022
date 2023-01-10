6bbae5a2d1b087148e3f94b1a94981202bccc12b
Patti SreeLatha <patti.sreelatha@gmail.com>
2023-01-10 10:41:17 -0500

    by indira

---
+++ indira1/PythonTraining/check 0 innumber.py
@@ -0,0 +1,13 @@
+# check a given number has 0 in the number or not
+
+n = int(input('enter the value of n:'))
+
+s=str(n)
+z=str(0)
+
+if z in s:
+    print('zero is found')
+
+else:
+    print('zero is not found')
+
